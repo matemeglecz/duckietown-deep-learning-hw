20972a718d61261eadb629a6620ea636642f516d
Máté Meglécz <meglecz.mate@gmail.com>
2022-11-20 22:01:17 +0100

    cleanrl, and baseline

---
+++ gym-duckietown/learning/env.py
@@ -0,0 +1,41 @@
+import gym
+
+from wrappers import (
+    NormalizeWrapper,
+    ImgWrapper,
+    DtRewardWrapper,
+    ActionWrapper,
+    ResizeWrapper,
+    SteeringToWheelVelWrapper,
+)
+
+
+def launch_env(id=None):
+    env = None
+    if id is None:
+        from gym_duckietown.simulator import Simulator
+
+        env = Simulator(
+            seed=123,  # random seed
+            #map_name="map1",
+            max_steps=500001,  # we don't want the gym to reset itself
+            domain_rand=False,
+            camera_width=640,
+            camera_height=480,
+            accept_start_angle_deg=4,  # start close to straight
+            full_transparency=True,
+            distortion=True,
+            randomize_maps_on_reset=True
+        )
+    else:
+        env = gym.make(id)
+
+    # Wrappers
+    env = ResizeWrapper(env)
+    env = NormalizeWrapper(env)
+    env = ImgWrapper(env)  # to make the images from 160x120x3 into 3x160x120
+    env = SteeringToWheelVelWrapper(env)
+    env = ActionWrapper(env)
+    #env = DtRewardWrapper(env)
+
+    return env
---
+++ gym-duckietown/learning/map_move_to_duckietown.py
@@ -0,0 +1,23 @@
+import site
+import os
+import shutil
+
+print(site.getsitepackages())
+
+dest_path = site.getsitepackages()[0]
+
+dest_path = dest_path + '/duckietown_world/data/gd1/maps'
+
+src_path = './gym_duckietown/maps'
+files = os.listdir(src_path)
+
+
+for fname in files:
+     
+    # copying the files to the
+    # destination directory
+    shutil.copy2(os.path.join(src_path,fname), dest_path)
+
+
+
+
